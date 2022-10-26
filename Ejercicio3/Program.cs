816ca899ca295cc673ecff5a775cba866d71a0fb
Eunice Ricalday <e.ricalday.info@gmail.com>
2022-10-26 10:34:23 -0500

    numeros de del 0 al 100

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class ejercicio3
+{
+    static void Main()
+    {
+        int i, impar = 0, par = 0;
+        //Mostrar los números del 0 al 100
+        for(i= 0;i<101;i++)
+        {
+               Console.Write("{0}|",i);
+        }
+       
+        Console.Write("\n\n");
+    }
+}   
---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ejercicio3
+{
+    static void Main()
+    {
+        int i, impar = 0, par = 0;
+        //Mostrar los números del 100 al 0
+        for (i = 100; i > -1; i--)
+        {
+            Console.Write("{0}|", i);
+        }
+
+        Console.Write("\n\n");
+        
+    }
+}   
