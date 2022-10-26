8edef0e9211993eb9c1f29882c456e76db116dc5
Eunice Ricalday <e.ricalday.info@gmail.com>
2022-10-26 13:06:53 -0500

    numeros de del 0 al 100

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class ejercicio
+{
+    static void Main()
+    {
+        int i;
+
+        //Mostrar los multiplos de 3 del 0 al 100
+        for(i = 0; i < 100; i++)
+        {  
+            if(i % 3 == 0)
+            {
+                Console.Write("{0}|",i);
+            }    
+        }      
+
+
+        Console.Write("\n\n");
+
+    }
+}    
---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class ejercicio
+{
+    static void Main()
+    {
+        int i;
+
+        //Mostrar los multiplos de 3 y de 2 entre el 0 y 100
+        for(i = 0; i < 100; i++)
+        {  
+            if(i % 3 == 0 || i % 2 == 0)
+            {
+                Console.Write("{0}|",i);
+            }    
+        }      
+
+
+        Console.Write("\n\n");
+
+    }
+}   
